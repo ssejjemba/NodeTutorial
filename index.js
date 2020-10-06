6f0fa6baeadf09f2ff76f9a88cf2fa9f30a9ab41
Daniel Ssejjemba <daniel@conceptsauce.io>
2020-10-06 20:14:15 +0300

    Store user data in file

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
-var handlers = require('./handlers');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
 // Instantiating the HTTP server
 var httpServer = http.createServer(function (req, res) {
@@ -78,7 +79,7 @@ var unifiedServer = function (req, res) {
             queryStringObject: queryStringObject,
             method: method,
             headers: headers,
-            payload: buffer,
+            payload: helpers.parseBufferToJSON(buffer),
         };
 
         // Route the request to the handler specified in the router
