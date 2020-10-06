d254408fd3db9a41d4686939d08966c624574663
Daniel Ssejjemba <daniel@conceptsauce.io>
2020-10-06 12:46:04 +0300

    Start a server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+/**
+ * Primary file for the API
+ */
+
+//  Dependencies
+var http = require('http');
+
+// The server should respond to all requests with a string
+var server = http.createServer(function (req, res) {
+    res.end('Hello World\n');
+});
+
+// start the server, and have it listen to port 3000
+server.listen(3000, function () {
+    console.log('The server is listening on port 3000 now');
+});
