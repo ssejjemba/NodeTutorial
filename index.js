5ead4e164a0a334bcb941cb101c330066443d791
Daniel Ssejjemba <daniel@conceptsauce.io>
2020-10-06 18:28:20 +0300

    Add creating a file

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
 var fs = require('fs');
+var _data = require('./lib/data');
 
 // Instantiating the HTTP server
 var httpServer = http.createServer(function (req, res) {
