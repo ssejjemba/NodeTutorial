54bd65dce7640988f39dccc4325e9814200236d8
Daniel Ssejjemba <daniel@conceptsauce.io>
2020-10-06 13:26:27 +0300

    Parse query strings

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ var server = http.createServer(function (req, res) {
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\+|\/+$/g, '');
 
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
     // Get the HTTP Method
     var method = req.method.toLowerCase();
 
@@ -22,7 +25,14 @@ var server = http.createServer(function (req, res) {
     res.end('Hello World\n');
 
     // log the request path
-    console.log('Request recieved on path: ' + trimmedPath + ' with method: ' + method);
+    console.log(
+        'Request recieved on path: ' +
+            trimmedPath +
+            ' with method: ' +
+            method +
+            ' and with these query string parameters: ',
+        queryStringObject
+    );
 });
 
 // start the server, and have it listen to port 3000
